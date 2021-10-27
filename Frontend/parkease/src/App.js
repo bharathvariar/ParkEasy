12d9a55797753855121de0b55ad85a7656334006
legendsovermyths <maskwolf2000@gmail.com>
2021-10-27 11:25:39 +0530

    sign-up page done

--- Frontend/parkease/src/App.js
+++ Frontend/parkease/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import { HomePage } from "./HomePage";
+// import { HomePage } from "./HomePage";
+import Navbar from "./Navbar";
+import Signup from "./Signup";
 
 function App() {
   return (
     <div className='App'>
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      <Navbar></Navbar>
+      <Signup></Signup>
     </div>
   );
 }
