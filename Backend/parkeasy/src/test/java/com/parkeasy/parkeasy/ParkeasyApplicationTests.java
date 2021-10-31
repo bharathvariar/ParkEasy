663a33c0c1840383687f0e6c4d65cae99a8d5684
Bharath Variar <bharathvariar@gmail.com>
2021-10-31 11:43:10 +0530

    Backend folder created

---
+++ Backend/parkeasy/src/test/java/com/parkeasy/parkeasy/ParkeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkeasy.parkeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
