f51b3a2ab74a065c2e1a2b133692ca78b6e10448
Bharath Variar <bharathvariar@gmail.com>
2021-11-02 18:16:28 +0530

    Added basic security, using spring-boot-security dependency

---
+++ Backend/parkeasy/src/main/java/com/parkeasy/parkeasy/HomeResourceController.java
@@ -0,0 +1,26 @@
+package com.parkeasy.parkeasy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResourceController {
+
+    @GetMapping("/")
+    // Accessible by everyone
+    public String Home() {
+        return "<h1> Welcome </h1>";
+    }
+
+    @GetMapping("/user")
+    // Accessible only by USER and ADMIN roles
+    public String User() {
+        return "<h1> Welcome User </h1>";
+    }
+
+    @GetMapping("/admin")
+    // Accessible only by ADMIN roles
+    public String Admin() {
+        return "<h1> Welcome Admin </h1>";
+    }
+}
