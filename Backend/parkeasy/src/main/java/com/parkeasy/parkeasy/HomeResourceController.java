b6555e307b49385383c9c0e2a5af51c7fa42fbcd
Bharath Variar <bharathvariar@gmail.com>
2021-11-03 23:35:04 +0530

    minor devolopments in HomeResourceController

--- Backend/parkeasy/src/main/java/com/parkeasy/parkeasy/HomeResourceController.java
+++ Backend/parkeasy/src/main/java/com/parkeasy/parkeasy/HomeResourceController.java
@@ -1,10 +1,17 @@
 package com.parkeasy.parkeasy;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeResourceController {
+
+    @Autowired
+    userRepository UserRepo;
 
     @GetMapping("/")
     // Accessible by everyone
@@ -22,5 +29,19 @@ public class HomeResourceController {
     // Accessible only by ADMIN roles
     public String Admin() {
         return "<h1> Welcome Admin </h1>";
+    }
+
+    @PostMapping(path = "/user", consumes = { "application/json" })
+    public ResponseEntity<?> addUser(@RequestBody AuthenticationRequest AuthenticationRequest) {
+        String username = AuthenticationRequest.getUsername();
+        String password = AuthenticationRequest.getPassword();
+        userModel user = new userModel(username, password);
+        try {
+            UserRepo.save(user);
+        } catch (Exception e) {
+            return ResponseEntity.ok(new AuthenticationResponse("Error SignIn Failed"));
+        }
+        
+        return ResponseEntity.ok(new AuthenticationResponse("Welcome" + username));
     }
 }
