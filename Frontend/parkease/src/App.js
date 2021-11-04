666e126811e8320071245733181e269822f90f06
legendsovermyths <maskwolf2000@gmail.com>
2021-11-04 13:57:15 +0530

    payment page

--- Frontend/parkease/src/App.js
+++ Frontend/parkease/src/App.js
@@ -4,7 +4,8 @@ import Navbar from "./Navbar";
 import Signup from "./Signup";
 import SignIn from "./Signin";
 import PricingContent from "./Pricing";
-// import AddressForm from "./Payment/AddressForm";
+import AddressForm from "./Payment/AddressForm";
+import Checkout from "./Payment/Checkout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
           <Route path='/Signup' component={Signup} />
           <Route path='/Signin' component={SignIn} />
           <Route path='/Pricing' component={PricingContent} />
-          {/* <Route path='/Payment' component={AddressForm} /> */}
+          <Route path='/Checkout' component={Checkout} />
         </Switch>
       </div>
     </Router>
