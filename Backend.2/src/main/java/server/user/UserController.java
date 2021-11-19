d8016d756d90c04b912e293fe61d5fc8fa9b2ea3
legendsovermyths <maskwolf2000@gmail.com>
2021-11-19 23:48:45 +0530

    backend2

---
+++ Backend.2/src/main/java/server/user/UserController.java
@@ -0,0 +1,68 @@
+package server.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @CrossOrigin()
+    @PostMapping("/users/register")
+    public Status registerUser(@Valid @RequestBody User newUser) {
+        List<User> users = userRepository.findAll();
+
+        for (User user : users) {
+            if (user.equals(newUser)) {
+                System.out.println("User Already exists!");
+                return Status.USER_ALREADY_EXISTS;
+            }
+        }
+
+        userRepository.save(newUser);
+        return Status.SUCCESS;
+    }
+
+    @CrossOrigin()
+    @PostMapping("/users/login")
+    public Status loginUser(@Valid @RequestBody User user) {
+        List<User> users = userRepository.findAll();
+
+        for (User other : users) {
+            if (other.equals(user)) {
+                user.setLoggedIn(true);
+                userRepository.save(user);
+                return Status.SUCCESS;
+            }
+        }
+
+        return Status.FAILURE;
+    }
+
+    @CrossOrigin()
+    @PostMapping("/users/logout")
+    public Status logUserOut(@Valid @RequestBody User user) {
+        List<User> users = userRepository.findAll();
+
+        for (User other : users) {
+            if (other.equals(user)) {
+                user.setLoggedIn(false);
+                userRepository.save(user);
+                return Status.SUCCESS;
+            }
+        }
+
+        return Status.FAILURE;
+    }
+
+    @CrossOrigin()
+    @DeleteMapping("/users/all")
+    public Status deleteUsers() {
+        userRepository.deleteAll();
+        return Status.SUCCESS;
+    }
+}
